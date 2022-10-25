9558fde5e1f6f5e36a71d459dfb16d466b861ff9
Renato Macedo <renato_macedo_1@hotmail.com>
2022-10-25 18:41:21 +0100

    search form

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -65,6 +65,12 @@ const allJobsSlice = createSlice({
     hideLoading: (state) => {
       state.isLoading = false;
     },
+    handleChange: (state, { payload: { name, value } }) => {
+      state[name] = value;
+    },
+    clearFilters: (state) => {
+      return { ...state, ...initialFiltersState };
+    },
   },
   extraReducers: {
     [getAllJobs.pending]: (state) => {
@@ -93,6 +99,7 @@ const allJobsSlice = createSlice({
   },
 });
 
-export const { showLoading, hideLoading } = allJobsSlice.actions;
+export const { showLoading, hideLoading, handleChange, clearFilters } =
+  allJobsSlice.actions;
 
 export default allJobsSlice.reducer;
